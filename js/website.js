1251d6a9a25ebaad89492ebe18044df77ac42ed0
simpulton <simpul@gmail.com>
2013-06-14 16:13:01 -0700

    Clean up with state!

--- js/website.js
+++ js/website.js
@@ -1,23 +1,54 @@
 angular.module('website', []).
-    config(function($routeProvider) {
+    config(function ($routeProvider) {
         $routeProvider.
-            when('/about', {templateUrl:'partials/basic-template.html', controller:AboutCtrl}).
-            when('/experiments', {templateUrl:'partials/basic-template.html', controller:ExperimentsCtrl   }).
-            when('/home', {templateUrl:'partials/basic-template.html', controller:HomeCtrl   }).
-            otherwise({redirectTo:'/home'});
+            when('/about', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'}).
+            when('/experiments', {templateUrl: 'partials/experiments.html', controller: 'ExperimentsCtrl'}).
+            when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'}).
+            otherwise({redirectTo: '/home'});
+    })
+    .controller('AboutCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+        $scope.title = 'About Page';
+        $scope.body = 'This is the about page body';
+
+        $scope.message = StateService.getMessage();
+
+        $scope.updateMessage = function (m) {
+            StateService.setMessage(m);
+        };
+    }])
+    .controller('ExperimentsCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+        $scope.title = 'Experiments Page';
+        $scope.body = 'This is the about experiments body';
+
+        $scope.message = StateService.getMessage();
+
+        $scope.updateMessage = function (m) {
+            StateService.setMessage(m);
+        };
+    }])
+    .controller('HomeCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+        $scope.title = 'Home Page';
+        $scope.body = 'This is the about home body';
+
+        $scope.message = StateService.getMessage();
+
+        $scope.updateMessage = function (m) {
+            StateService.setMessage(m);
+        };
+    }])
+    .factory('StateService', function () {
+        var message = 'Hello Message';
+        var getMessage = function() {
+            return message;
+        };
+        var setMessage = function(m) {
+            message = m;
+
+            console.log('Message Yo!', message);
+        };
+
+        return {
+            getMessage: getMessage,
+            setMessage: setMessage
+        }
     });
-
-function AboutCtrl($scope) {
-    $scope.title = 'About Page';
-    $scope.body = 'This is the about page body';
-}
-
-function ExperimentsCtrl($scope) {
-    $scope.title = 'Experiments Page';
-    $scope.body = 'This is the about experiments body';
-}
-
-function HomeCtrl($scope) {
-    $scope.title = 'Home Page';
-    $scope.body = 'This is the about home body';
-}
