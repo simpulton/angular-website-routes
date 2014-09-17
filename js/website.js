27e561d77baaff4acfedda4e7994824d93e8f9ab
Lukas Ruebbelke <simpul@gmail.com>
2014-09-17 08:24:18 -0500

    With directives!

--- js/website.js
+++ js/website.js
@@ -6,7 +6,7 @@ angular.module('website', ['ngRoute']).
             when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'}).
             otherwise({redirectTo: '/home'});
     })
-    .controller('AboutCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+    .controller('AboutCtrl', function ($scope, StateService) {
         $scope.title = 'About Page';
         $scope.body = 'This is the about page body';
 
@@ -15,18 +15,19 @@ angular.module('website', ['ngRoute']).
         $scope.updateMessage = function (m) {
             StateService.setMessage(m);
         };
-    }])
-    .controller('ExperimentsCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+    })
+    .controller('ExperimentsCtrl', function ($scope, StateService, ExperimentsService) {
         $scope.title = 'Experiments Page';
         $scope.body = 'This is the about experiments body';
 
         $scope.message = StateService.getMessage();
+        $scope.experiments = ExperimentsService.getExperiments();
 
         $scope.updateMessage = function (m) {
             StateService.setMessage(m);
         };
-    }])
-    .controller('HomeCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+    })
+    .controller('HomeCtrl', function ($scope, StateService) {
         $scope.title = 'Home Page';
         $scope.body = 'This is the about home body';
 
@@ -35,18 +36,58 @@ angular.module('website', ['ngRoute']).
         $scope.updateMessage = function (m) {
             StateService.setMessage(m);
         };
-    }])
+    })
     .factory('StateService', function () {
         var message = 'Hello Message';
-        var getMessage = function() {
+        var getMessage = function () {
             return message;
         };
-        var setMessage = function(m) {
+        var setMessage = function (m) {
             message = m;
         };
 
         return {
             getMessage: getMessage,
             setMessage: setMessage
+        }
+    })
+    .service('ExperimentsService', function () {
+        var service = this,
+            experiments = [
+                {name: 'Experiment 1', description: 'This is an experiment', completed:0},
+                {name: 'Experiment 2', description: 'This is an experiment', completed:0},
+                {name: 'Experiment 3', description: 'This is an experiment', completed:0},
+                {name: 'Experiment 4', description: 'This is an experiment', completed:0}
+            ];
+
+        service.getExperiments = function() {
+            return experiments;
+        };
+    })
+    .directive('experiment', function(){
+        var linker = function (scope, element, attrs) {
+            element.on('click', function(){
+                scope.doExperiment();
+            })
+        };
+
+        var controller =  function($scope){
+            $scope.doExperiment = function() {
+                $scope.$apply(function(){
+                    $scope.experiment.completed++;
+                });
+            };
+        };
+
+        return {
+            scope: true,
+            restrict: 'E',
+            template: '<div class="experiment">' +
+                '<h3>{{experiment.name}}</h3>' +
+                '<p>{{experiment.description}}</p>' +
+                '<p><strong>{{experiment.completed}}</strong></p>' +
+                '</div>',
+            link: linker,
+            controller: controller
         }
     });
