b5826056fd2e8691905f4b4dc1731eada9d2d4b9
Jonathan Garvey <jon.d.garvey@gmail.com>
2016-04-15 14:50:32 -0700

    adding ui router and animations

--- js/website.js
+++ js/website.js
@@ -1,93 +1,138 @@
-angular.module('website', ['ngRoute']).
-    config(function ($routeProvider) {
-        $routeProvider.
-            when('/about', {templateUrl: 'partials/about.html', controller: 'AboutCtrl'}).
-            when('/experiments', {templateUrl: 'partials/experiments.html', controller: 'ExperimentsCtrl'}).
-            when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'}).
-            otherwise({redirectTo: '/home'});
-    })
-    .controller('AboutCtrl', function ($scope, StateService) {
-        $scope.title = 'About Page';
-        $scope.body = 'This is the about page body';
+angular.module('website', ['ui.router', 'ngAnimate'])
+  .value('animations', {
+    fade: {
+      enter: function (element, done) {
+        TweenMax.to(element, 0.5, {autoAlpha: 1, onComplete: done, ease:Linear.easeNone});
+      },
+      leave: function (element, done) {
+        TweenMax.to(element, 0.5, {autoAlpha: 0, onComplete: done, ease:Linear.easeNone});
+      }
+    },
+    slide: {
+      enter: function (element, done) {
+        TweenMax.fromTo(element, 0.5, {autoAlpha: 0, left: -element[0].clientWidth}, {autoAlpha: 1, left: 0, onComplete: done, ease:Bounce.easeIn});
+      },
+      leave: function (element, done) {
+        TweenMax.to(element, 0.5, {autoAlpha: 0, left: -element[0].clientWidth, onComplete: done, ease:Linear.easeNone});
+      }
+    }
+  })
+  .animation('.main', function (animations) {
+    // return animations.fade;
+    return animations.slide;
+  })
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'partials/about.html',
+        controller: 'AboutCtrl'
+      })
+      .state('experiments', {
+        url: '/experiments',
+        templateUrl: 'partials/experiments.html',
+        controller: 'ExperimentsCtrl'
+      })
+      .state('home', {
+        url: '/home',
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl'
+      });
+  })
+  .controller('AboutCtrl', function($scope, StateService) {
+    $scope.title = 'About Page';
+    $scope.body = 'This is the about page body';
 
-        $scope.message = StateService.getMessage();
+    $scope.message = StateService.getMessage();
 
-        $scope.updateMessage = function (m) {
-            StateService.setMessage(m);
-        };
-    })
-    .controller('ExperimentsCtrl', function ($scope, StateService, ExperimentsService) {
-        $scope.title = 'Experiments Page';
-        $scope.body = 'This is the about experiments body';
+    $scope.updateMessage = function(m) {
+      StateService.setMessage(m);
+    };
+  })
+  .controller('ExperimentsCtrl', function($scope, StateService, ExperimentsService) {
+    $scope.title = 'Experiments Page';
+    $scope.body = 'This is the about experiments body';
 
-        $scope.message = StateService.getMessage();
-        $scope.experiments = ExperimentsService.getExperiments();
+    $scope.message = StateService.getMessage();
+    $scope.experiments = ExperimentsService.getExperiments();
 
-        $scope.updateMessage = function (m) {
-            StateService.setMessage(m);
-        };
-    })
-    .controller('HomeCtrl', function ($scope, StateService) {
-        $scope.title = 'Home Page';
-        $scope.body = 'This is the about home body';
+    $scope.updateMessage = function(m) {
+      StateService.setMessage(m);
+    };
+  })
+  .controller('HomeCtrl', function($scope, StateService) {
+    $scope.title = 'Home Page';
+    $scope.body = 'This is the about home body';
 
-        $scope.message = StateService.getMessage();
+    $scope.message = StateService.getMessage();
 
-        $scope.updateMessage = function (m) {
-            StateService.setMessage(m);
-        };
-    })
-    .factory('StateService', function () {
-        var message = 'Hello Message';
-        var getMessage = function () {
-            return message;
-        };
-        var setMessage = function (m) {
-            message = m;
-        };
+    $scope.updateMessage = function(m) {
+      StateService.setMessage(m);
+    };
+  })
+  .factory('StateService', function() {
+    var message = 'Hello Message';
+    var getMessage = function() {
+      return message;
+    };
+    var setMessage = function(m) {
+      message = m;
+    };
 
-        return {
-            getMessage: getMessage,
-            setMessage: setMessage
-        }
-    })
-    .service('ExperimentsService', function () {
-        var service = this,
-            experiments = [
-                {name: 'Experiment 1', description: 'This is an experiment', completed:0},
-                {name: 'Experiment 2', description: 'This is an experiment', completed:0},
-                {name: 'Experiment 3', description: 'This is an experiment', completed:0},
-                {name: 'Experiment 4', description: 'This is an experiment', completed:0}
-            ];
+    return {
+      getMessage: getMessage,
+      setMessage: setMessage
+    }
+  })
+  .service('ExperimentsService', function() {
+    var service = this,
+      experiments = [{
+        name: 'Experiment 1',
+        description: 'This is an experiment',
+        completed: 0
+      }, {
+        name: 'Experiment 2',
+        description: 'This is an experiment',
+        completed: 0
+      }, {
+        name: 'Experiment 3',
+        description: 'This is an experiment',
+        completed: 0
+      }, {
+        name: 'Experiment 4',
+        description: 'This is an experiment',
+        completed: 0
+      }];
 
-        service.getExperiments = function() {
-            return experiments;
-        };
-    })
-    .directive('experiment', function(){
-        var linker = function (scope, element, attrs) {
-            element.on('click', function(){
-                scope.doExperiment();
-            })
-        };
+    service.getExperiments = function() {
+      return experiments;
+    };
+  })
+  .directive('experiment', function() {
+    var linker = function(scope, element, attrs) {
+      element.on('click', function() {
+        scope.doExperiment();
+      })
+    };
 
-        var controller =  function($scope){
-            $scope.doExperiment = function() {
-                $scope.$apply(function(){
-                    $scope.experiment.completed++;
-                });
-            };
-        };
+    var controller = function($scope) {
+      $scope.doExperiment = function() {
+        $scope.$apply(function() {
+          $scope.experiment.completed++;
+        });
+      };
+    };
 
-        return {
-            scope: true,
-            restrict: 'E',
-            template: '<div class="experiment">' +
-                '<h3>{{experiment.name}}</h3>' +
-                '<p>{{experiment.description}}</p>' +
-                '<p><strong>{{experiment.completed}}</strong></p>' +
-                '</div>',
-            link: linker,
-            controller: controller
-        }
-    });
+    return {
+      scope: true,
+      restrict: 'E',
+      template: '<div class="experiment">' +
+        '<h3>{{experiment.name}}</h3>' +
+        '<p>{{experiment.description}}</p>' +
+        '<p><strong>{{experiment.completed}}</strong></p>' +
+        '</div>',
+      link: linker,
+      controller: controller
+    }
+  });
